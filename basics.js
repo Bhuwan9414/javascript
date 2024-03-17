0a23d92b01c240bb6b032c15c162816b4c3eee41
Bhuwan001 <115343236+Bhuwan9414@users.noreply.github.com>
2024-03-17 11:41:08 +0530

    Add files via upload
    Datatype conversion

---
+++ basics.js
@@ -0,0 +1,44 @@
+// const accountId = 112233
+// let accountEmail = "sutharbhuwan2@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "jaipur"
+// let accountState;
+
+
+// accountEmail = "jsjj@gmail.com"
+// accountPassword = "78686"
+// accountCity = "Bengaluru"
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// let name = "hitesh"
+// let age = 18
+// let isloggedin = false
+
+// console.log(typeof "hitesh");
+
+
+// console.log(3+3);
+// console.log("bhuwan");
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
